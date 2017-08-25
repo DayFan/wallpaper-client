eb6d72a130d693805ba54c4082a90b8993225f90
Andrey Skvortsov <medif625@gmail.com>
2017-08-25 21:06:11 +0300

    Added function for set wallpaper on windows

--- wallpaper/wallpaper_windows.go
+++ wallpaper/wallpaper_windows.go
@@ -1,10 +1,22 @@
 package wallpaper
 
-import "fmt"
+import (
+	"syscall"
+	"unsafe"
+)
 
 //Set is function for set wallpaper
 func Set(path string) error {
-	fmt.Println(path)
+	var mod = syscall.NewLazyDLL("user32.dll")
+	var proc = mod.NewProc("SystemParametersInfoW")
+	var SPI_SETDESKWALLPAPER = 0x0014
+	var SPIF_UPDATEINIFILE = 0x01
+
+	proc.Call(
+		uintptr(SPI_SETDESKWALLPAPER),
+		0,
+		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(path))),
+		uintptr(SPIF_UPDATEINIFILE))
 
 	return nil
 }
