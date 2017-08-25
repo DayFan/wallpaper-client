2cea15c4739646c1506e7e24349de800186f4d4d
Andrey Skvortsov <medif625@gmail.com>
2017-08-25 23:12:16 +0300

    Added output of the resulting message after set the wallpaper

--- wallpaper/wallpaper_windows.go
+++ wallpaper/wallpaper_windows.go
@@ -1,22 +1,26 @@
 package wallpaper
 
 import (
+	"log"
 	"syscall"
 	"unsafe"
 )
+
+const SPI_SETDESKWALLPAPER = 0x0014
+const SPIF_UPDATEINIFILE = 0x01
 
 //Set is function for set wallpaper
 func Set(path string) error {
 	var mod = syscall.NewLazyDLL("user32.dll")
 	var proc = mod.NewProc("SystemParametersInfoW")
-	var SPI_SETDESKWALLPAPER = 0x0014
-	var SPIF_UPDATEINIFILE = 0x01
 
-	proc.Call(
+	_, _, err := proc.Call(
 		uintptr(SPI_SETDESKWALLPAPER),
 		0,
 		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(path))),
 		uintptr(SPIF_UPDATEINIFILE))
+
+	log.Printf("Result message - %s", err.Error())
 
 	return nil
 }
